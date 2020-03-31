f466bce3e7f3abe76e682108ed6f7c65e1276cd9
wangyongfeng@kuaishou.com <imaoda@126.com>
2020-03-31 16:34:30 +0800

    feat: 增加git reset --soft 指令

--- dev.js
+++ dev.js
@@ -6,7 +6,7 @@ const scripts = require(path.join(process.cwd(), 'package.json')).scripts || {}
 
 const command = scripts.dev ? 'dev' : scripts.serve ? 'serve' : 'start'
 args.unshift(command)
-args.push('--color') // 确保 stream 输出到终端的时候能显示正确的颜色
+// args.push('--color') // 确保 stream 输出到终端的时候能显示正确的颜色
 
 const gm = spawn("yarn", args);
 
---
+++ git.js
@@ -0,0 +1,12 @@
+const { execSync, spawn } = require("child_process");
+
+module.exports = function () {
+    try {
+      execSync("which git");
+    } catch (error) {
+      console.log("error: 您的 pc 未安装 git");
+      process.exit(1);
+    }
+    
+    execSync("git reset --soft HEAD~1");
+  }
